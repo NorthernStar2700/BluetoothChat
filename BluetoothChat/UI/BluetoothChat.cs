77c95878e519a20300d0536b39741afb32449ca0
NorthernStar2700 <hashimqureshi.dev@gmail.com>
2026-06-22 00:30:37 -0400

    Remove un-used using statements

--- BluetoothChat/UI/BluetoothChat.cs
+++ BluetoothChat/UI/BluetoothChat.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Security.Principal;
 using System.Windows.Forms;
 using BluetoothChat.Constants;
 using BluetoothChat.Enums;
