d432248b760d8a7861cf07ac069e7c68e9cbf9a1
NorthernStar2700 <hashimqureshi.dev@gmail.com>
2026-07-10 07:20:23 -0400

    Update secure message protocol
    Use the same IV in encrypt and decrypt and wait for all the data to finish being processed before returning it

--- BluetoothChat/Functions/SecureMessageProtocol.cs
+++ BluetoothChat/Functions/SecureMessageProtocol.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BluetoothChat.Functions
@@ -9,6 +10,15 @@ namespace BluetoothChat.Functions
     {
         public static async Task<byte[]> EncryptAsync(string message, byte[] aesKey)
         {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new IOException("Message content is missing or empty.");
+            }
+            if (aesKey == null || aesKey.Length == 0)
+            {
+                throw new IOException("AES key is missing or empty.");
+            }
+
             using (Aes aes = Aes.Create())
             {
                 aes.Key = aesKey;
@@ -17,39 +27,79 @@ namespace BluetoothChat.Functions
 
                 aes.GenerateIV();
                 byte[] iv = aes.IV;
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                byte[] encryptedMessageBytes;
 
                 using (ICryptoTransform encryptor = aes.CreateEncryptor())
                 using (MemoryStream memory = new MemoryStream())
                 using (CryptoStream crypt = new CryptoStream(memory, encryptor, CryptoStreamMode.Write))
-                using (StreamWriter writer = new StreamWriter(crypt))
                 {
-                    await writer.WriteAsync(message);
-                    return memory.ToArray();
+                    // Finish writing everything before returning the result
+                    await crypt.WriteAsync(messageBytes, 0, messageBytes.Length);
+                    crypt.FlushFinalBlock();
+
+                    encryptedMessageBytes = memory.ToArray();
                 }
+
+                // Use the same initialization vector
+                byte[] result = Combine(iv, encryptedMessageBytes);
+                return result;
             }
         }
 
         public static async Task<string> DecryptAsync(string message, byte[] aesKey)
         {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new IOException("Message content is missing or empty.");
+
+            }
+            if (aesKey == null || aesKey.Length == 0)
+            {
+                throw new IOException("AES key is missing or empty.");
+            }
+
+            byte[] dataToDecrypt = Convert.FromBase64String(message);
+
             using (Aes aes = Aes.Create())
             {
+                int ivLength = aes.BlockSize / 8;
+                byte[] iv = new byte[ivLength];
+                byte[] decryptedText = new byte[dataToDecrypt.Length - ivLength];
+
                 aes.Key = aesKey;
                 aes.Mode = CipherMode.CBC;
                 aes.Padding = PaddingMode.PKCS7;
+                aes.IV = iv;
 
-                aes.GenerateIV();
+                // IV byte array now has data (IV = 0 to IV.Length)
+                Buffer.BlockCopy(dataToDecrypt, 0, iv, 0, ivLength);
 
-                byte[] dataToDecrypt = Convert.FromBase64String(message);
+                // Text byte array now has data (Message = IV.Length to decryptedText.Length)
+                Buffer.BlockCopy(dataToDecrypt, ivLength, decryptedText, 0, decryptedText.Length);
 
                 using (ICryptoTransform decryptor = aes.CreateDecryptor())
                 using (MemoryStream memory = new MemoryStream(dataToDecrypt))
                 using (CryptoStream crypt = new CryptoStream(memory, decryptor, CryptoStreamMode.Read))
-                using (StreamReader reader = new StreamReader(crypt))
+                using (MemoryStream output = new MemoryStream())
                 {
-                    string result = await reader.ReadToEndAsync();
-                    return result;
+                    await crypt.CopyToAsync(output);
+                    return Encoding.UTF8.GetString(output.ToArray());
                 }
             }
+        }
+
+        private static byte[] Combine(byte[] array1, byte[] array2)
+        {
+            byte[] combinedBytes = new byte[array1.Length + array2.Length];
+
+            // Copy array 1's contents to the new array
+            Buffer.BlockCopy(array1, 0, combinedBytes, 0, array1.Length);
+
+            // Copy array 2's contents to the new array starting at the end of array 1's length
+            Buffer.BlockCopy(array2, 0, combinedBytes, array1.Length, array2.Length);
+
+            return combinedBytes;
         }
     }
 }
